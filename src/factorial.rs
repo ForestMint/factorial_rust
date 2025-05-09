c1d4fd8ef9a4cc885db73ec68b1625cbda328d9b
desdames-nicolas <n.desdames@gmail.com>
2025-05-09 21:55:17 +0200

    benchmark for fibonacci is added

--- src/factorial.rs
+++ src/factorial.rs
@@ -15,26 +15,9 @@ pub fn calculate_factorial_recursively (n: i64) -> i64 {
     }
 }
 
-pub fn calculate_fibonacci_with_loop (mut n: i64) -> i64 {
-    let mut result = 1;
-    let mut value_following_result = 1;
-    while n>1 {
-        let mem = value_following_result;
-        value_following_result+=result;
-        result = mem;
-        n-=1;
-    }
-    result
-}
 
-pub fn calculate_fibonacci_recursively (n: i64)  -> i64 {
-    match n {
-        1 => 1,
-        2 => 1,
-        // Handle the rest of cases
-        _ => calculate_fibonacci_recursively(n-2)+calculate_fibonacci_recursively(n-1),
-    }
-}
+
+
 
 #[cfg(test)]
 mod tests {
@@ -61,27 +44,4 @@ mod tests {
         assert_eq!(calculate_factorial_with_loop(5), 120);
     }
 
-    #[test]
-    fn test_calculate_fibonacci_recursively() {
-        assert_eq!(calculate_fibonacci_recursively(1), 1);
-        assert_eq!(calculate_fibonacci_recursively(2), 1);
-        assert_eq!(calculate_fibonacci_recursively(3), 2);
-        assert_eq!(calculate_fibonacci_recursively(4), 3);
-        assert_eq!(calculate_fibonacci_recursively(5), 5);
-        assert_eq!(calculate_fibonacci_recursively(6), 8);
-        assert_eq!(calculate_fibonacci_recursively(7), 13);
-        assert_eq!(calculate_fibonacci_recursively(8), 21);
-    }
-
-    #[test]
-    fn test_calculate_fibonacci_with_loop() {
-        assert_eq!(calculate_fibonacci_with_loop(1), 1);
-        assert_eq!(calculate_fibonacci_with_loop(2), 1);
-        assert_eq!(calculate_fibonacci_with_loop(3), 2);
-        assert_eq!(calculate_fibonacci_with_loop(4), 3);
-        assert_eq!(calculate_fibonacci_with_loop(5), 5);
-        assert_eq!(calculate_fibonacci_with_loop(6), 8);
-        assert_eq!(calculate_fibonacci_with_loop(7), 13);
-        assert_eq!(calculate_fibonacci_with_loop(8), 21);
-    }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,32 +3,50 @@ use std::time::UNIX_EPOCH;
 use std::time::SystemTime;
 
 mod factorial;
+mod fibonacci;
 
 use factorial::calculate_factorial_with_loop;
 use factorial::calculate_factorial_recursively;
+use fibonacci::calculate_fibonacci_with_loop;
+use fibonacci::calculate_fibonacci_recursively;
 
 fn main () {
 
-    let number_of_iterations = 10000000;
-    let parameter = 20;
+    let number_of_iterations_factorial = 10000000;
+    let parameter_factorial = 20;
+    let number_of_iterations_fibonacci = 1000;
+    let parameter_fibonacci = 20;
 
     let start = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
-    for _ in 0..number_of_iterations {
+    for _ in 0..number_of_iterations_factorial {
         //do my stuff
-        let res_1 = calculate_factorial_with_loop(parameter);
+        let res_1 = calculate_factorial_with_loop(parameter_factorial);
     }
     let end = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
     println!("{:?}", end-start);
 
     let start_2 = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
-    for _ in 0..number_of_iterations {
+    for _ in 0..number_of_iterations_factorial {
         //do my stuff
-        let res_2 = calculate_factorial_recursively(parameter);
+        let res_2 = calculate_factorial_recursively(parameter_factorial);
     }
     let end_2 = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
     println!("{:?}", end_2-start_2);
 
+    let start = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
+    for _ in 0..number_of_iterations_fibonacci {
+        //do my stuff
+        let res_1 = calculate_fibonacci_with_loop(parameter_fibonacci);
+    }
+    let end = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
+    println!("{:?}", end-start);
 
-    //println!("{}", res_1);
-    //println!("{}", res_2);
+    let start_2 = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
+    for _ in 0..number_of_iterations_fibonacci {
+        //do my stuff
+        let res_2 = calculate_fibonacci_recursively(parameter_fibonacci);
+    }
+    let end_2 = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
+    println!("{:?}", end_2-start_2);
+
 }
