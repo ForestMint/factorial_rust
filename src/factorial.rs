3f973c309bccac1cec4ca9ca22b8fc9ca3272604
desdames-nicolas <n.desdames@gmail.com>
2025-05-09 21:46:26 +0200

    feat: function to calculate fibonacci with loop added*

--- src/factorial.rs
+++ src/factorial.rs
@@ -15,6 +15,18 @@ pub fn calculate_factorial_recursively (n: i64) -> i64 {
     }
 }
 
+pub fn calculate_fibonacci_with_loop (mut n: i64) -> i64 {
+    let mut result = 1;
+    let mut value_following_result = 1;
+    while n>1 {
+        let mem = value_following_result;
+        value_following_result+=result;
+        result = mem;
+        n-=1;
+    }
+    result
+}
+
 pub fn calculate_fibonacci_recursively (n: i64)  -> i64 {
     match n {
         1 => 1,
@@ -59,5 +71,17 @@ mod tests {
         assert_eq!(calculate_fibonacci_recursively(6), 8);
         assert_eq!(calculate_fibonacci_recursively(7), 13);
         assert_eq!(calculate_fibonacci_recursively(8), 21);
+    }
+
+    #[test]
+    fn test_calculate_fibonacci_with_loop() {
+        assert_eq!(calculate_fibonacci_with_loop(1), 1);
+        assert_eq!(calculate_fibonacci_with_loop(2), 1);
+        assert_eq!(calculate_fibonacci_with_loop(3), 2);
+        assert_eq!(calculate_fibonacci_with_loop(4), 3);
+        assert_eq!(calculate_fibonacci_with_loop(5), 5);
+        assert_eq!(calculate_fibonacci_with_loop(6), 8);
+        assert_eq!(calculate_fibonacci_with_loop(7), 13);
+        assert_eq!(calculate_fibonacci_with_loop(8), 21);
     }
 }
